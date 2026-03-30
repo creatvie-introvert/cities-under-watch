e7508d2ac13b71bece46bebfa7bed3c0ca01f7cf
urbanIntrovert <trefoil72@gmail.com>
2026-03-30 01:39:28 +0100

    refactor: wrap announcement banner logic in init function

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,14 @@
-const announcementBanner = document.querySelector("[data-announcement-banner]");
-const announcementCloseButton = document.querySelector("[data-announcement-close]");
+function initAnnouncementBanner() {
+    const announcementBanner = document.querySelector("[data-announcement-banner]");
+    const announcementCloseButton = document.querySelector("[data-announcement-close]");
 
-if (announcementBanner && announcementCloseButton) {
+    if (!announcementBanner || !announcementCloseButton) {
+        return;
+    }
+
     announcementCloseButton.addEventListener("click", () => {
-        announcementBanner.style.display = "none"
-    })
+        announcementBanner.style.display = "none";
+    });
 }
+
+initAnnouncementBanner();
