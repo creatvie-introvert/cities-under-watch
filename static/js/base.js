5a95f49b40822167b4f73510a390766491c18d83
urbanIntrovert <trefoil72@gmail.com>
2026-03-30 01:34:40 +0100

    feat: add dismissable announcement banner

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,8 @@
-console.log('Cities Under Watch base.js loaded')
+const announcementBanner = document.querySelector("[data-announcement-banner]");
+const announcementCloseButton = document.querySelector("[data-announcement-close]");
+
+if (announcementBanner && announcementCloseButton) {
+    announcementCloseButton.addEventListener("click", () => {
+        announcementBanner.style.display = "none"
+    })
+}
