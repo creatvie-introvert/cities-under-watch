568c21f9d20972dd16de918087e02c6b41d4135d
urbanIntrovert <trefoil72@gmail.com>
2026-04-03 14:17:23 +0100

    feat: add initial shop all catalogue page

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('catalogue/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = (
+        Product.objects
+        .filter(is_active=True, collection__city__is_active=True)
+        .select_related('collection', 'collection__city')
+        .prefetch_related('images')
+    )
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/product_list.html', context)
