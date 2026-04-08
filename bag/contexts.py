e8da4ec1442cd739649b5ac46492e208f8d794fe
urbanIntrovert <trefoil72@gmail.com>
2026-04-08 18:30:16 +0100

    feat: add bag context processor and register it in settings

---
+++ bag/contexts.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+from django.conf import settings
+from products.models import Product
+
+
+def bag_contents(request):
+    bag_items = []
+    total = Decimal('0.00')
+    product_count = 0
+
+    bag = request.session.get('bag', {})
+
+    for item_id in bag.keys():
+        product = Product.objects.get(id=item_id)
+        total += product.price
+        product_count += 1
+
+        bag_items.append({
+            'item_id': item_id,
+            'product': product,
+        })
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
+    return context
