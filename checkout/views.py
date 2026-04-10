931d4c40871592b82671f1cc94b9430a58763798
urbanIntrovert <trefoil72@gmail.com>
2026-04-10 21:48:18 +0100

    feat: create checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+from bag.contexts import bag_contents
+from .forms import OrderForm
+
+
+def checkout(request):
+    """Render the checkout page."""
+    bag = request.session.get('bag', {})
+
+    if not bag:
+        messages.error(request, 'There is nothing in your basket')
+        return redirect(reverse("product_list"))
+
+    order_form = OrderForm()
+    current_bag = bag_contents(request)
+
+    context = {
+        'order_form': order_form,
+        'bag_items': current_bag['bag_items'],
+        'total': current_bag['total'],
+        'product_count': current_bag['product_count'],
+    }
+
+    return render(request, 'checkout/checkout.html', context)
