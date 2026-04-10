ed4ad0de31117979e2157f98d0cce0f35ad2895c
urbanIntrovert <trefoil72@gmail.com>
2026-04-10 20:20:27 +0100

    feat: register checkout models in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ("lineitem_total",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        'order_number',
+        'created_at',
+        'updated_at',
+        'order_total',
+        'grand_total',
+        'original_bag',
+        'stripe_pid',
+    )
+
+    fields = (
+        'order_number',
+        'full_name',
+        'email',
+        'phone_number',
+        'country_code',
+        'postcode',
+        'town_or_city',
+        'street_address1',
+        'street_address2',
+        'county',
+        'created_at',
+        'updated_at',
+        'order_total',
+        'grand_total',
+        'original_bag',
+        'stripe_pid',
+    )
+
+    list_display = (
+        'order_number',
+        'full_name',
+        'email',
+        'phone_number',
+        'town_or_city',
+        'created_at',
+        'order_total',
+        'grand_total',
+    )
+
+    ordering = ('-created_at',)
--- checkout/models.py
+++ checkout/models.py
@@ -45,7 +45,7 @@ class Order(models.Model):
     def _generate_order_number(self):
         """Generate a random, unique order number using UUID."""
         return uuid4().hex.upper()
-    
+
     def update_total(self):
         """Update grand total each time a line item is added."""
         self.order_total = self.lineitems.aggregate(
@@ -54,15 +54,16 @@ class Order(models.Model):
 
         self.grand_total = self.order_total
         self.save()
-    
+
     def save(self, *args, **kwargs):
         """
-        Override save method to set the order number if it hasn't been set already.
+        Override save method to set the order number if it hasn't been set
+        already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.order_number
 
@@ -96,6 +97,6 @@ class OrderLineItem(models.Model):
         """Override save method to set lineitem total."""
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
