5462bf72d8e83f03f53a77af4a922c47bb3c7e38
urbanIntrovert <trefoil72@gmail.com>
2026-04-16 13:19:43 +0100

    feat: add downloads page and secure account download links

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,4 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('orders/', views.profile_orders, name='profile_orders'),
+    path('downloads/', views.profile_downloads, name='profile_downloads'),
+    path(
+        'downloads/<int:download_id>/',
+        views.account_download_file,
+        name='account_download_file',
+    ),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import FileResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 from checkout.models import Order
+from products.models import ProductDownload
 
 
 @login_required
@@ -38,3 +40,44 @@ def profile_orders(request):
     }
 
     return render(request, 'profiles/profile_orders.html', context)
+
+
+@login_required
+def profile_downloads(request):
+    """Display all downloadable files purchased by the user."""
+    profile = request.user.userprofile
+
+    downloads = ProductDownload.objects.filter(
+        product__orderlineitem__order__user=request.user
+    ).distinct().order_by('product__title', 'sort_order')
+
+    context = {
+        'profile': profile,
+        'downloads': downloads,
+        'on_profile_page': True,
+    }
+
+    return render(request, 'profiles/profile_downloads.html', context)
+
+
+@login_required
+def account_download_file(request, download_id):
+    """Serve a purchased download file to the logged-in user."""
+    download = get_object_or_404(ProductDownload, pk=download_id)
+
+    has_access = Order.objects.filter(
+        user=request.user,
+        lineitems__product=download.product,
+    ).exists()
+
+    if not has_access:
+        raise Http404("You do not have access to this file.")
+
+    if not download.file:
+        raise Http404("File not found.")
+
+    return FileResponse(
+        download.file.open('rb'),
+        as_attachment=True,
+        filename=download.file.name.split('/')[-1],
+    )
